40f6771fcb32f849d324398425bc6e21a371b3ca
Anton Belousov <anton.belousov.v@mail.ru>
2022-04-04 23:26:39 +0300

    +test

--- cameraview/src/main/java/com/otaliastudios/cameraview/CameraUtils.java
+++ cameraview/src/main/java/com/otaliastudios/cameraview/CameraUtils.java
@@ -45,9 +45,10 @@ public class CameraUtils {
      */
     @SuppressWarnings("WeakerAccess")
     public static boolean hasCameras(@NonNull Context context) {
-        
+
         PackageManager manager = context.getPackageManager();
         // There's also FEATURE_CAMERA_EXTERNAL , should we support it?
+
         return manager.hasSystemFeature(PackageManager.FEATURE_CAMERA)
                 || manager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FRONT);
     }
